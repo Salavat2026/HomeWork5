987dcea178f3ffd7468290563e7799ed16c0b965
Salavat2026 <Salavat92@bk.ru>
2026-03-10 22:01:09 +0500

    Сделал с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Задача №1
+        System.out.println("Задача №1");
+        char clientOS = '0';
+        if (clientOS == '0') {
+            System.out.println("Для iOS — «Установите версию приложения для iOS по ссылке»");
+        } else {
+            System.out.println("Для Android — «Установите версию приложения для Android по ссылке».");
+        }
+        //Задача №2
+        System.out.println("Задача №2");
+        int clientDeviceYear = 2014;
+        if (clientOS == '0' && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOS == '1' && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для " + (clientOS == '0' ? "iOS" : "Android") + " по ссылке:");
+        }
+        //Задача №3
+        System.out.println("Задача №3");
+        int year = 1500;
+        if (year > 1584) {
+            if (((year % 4 == 0) && (year % 100 != 0))||(year % 400 == 0)){
+                System.out.println(year+" год является високосным");
+            }else {
+                System.out.println(year+" год не является високосным");
+            }
+        }
+        //Задача №4
+        System.out.println("Задача №4");
+        int deliveryDistance=19;
+        int deliveryDay;
 
+        }
     }
-}
